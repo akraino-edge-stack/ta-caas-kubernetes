5f8eb2f0e14bf53ddff3e70f8f11d26f040968f0
Balazs Szekeres <balazs.szekeres@nokia.com>
2019-11-12 14:52:08 +0100

    Kubelet healthcheck reimagined.
    Fix for: https://jira.akraino.org/browse/REC-71
    Healthcheck now pings local kubelet healtz api.
    Every loop now tries to uncordon the node (if the node is ready and cordoned)
    
    Change-Id: Ib7665f0864fbdd2feb4d5ab9116e7c34030ee3f4
    Signed-off-by: Balazs Szekeres <balazs.szekeres@nokia.com>

--- ansible/roles/kubelet/templates/kubelet_healthcheck.sh
+++ ansible/roles/kubelet/templates/kubelet_healthcheck.sh
@@ -13,53 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-wait_for_file () {
-  while [[ ! -f $1 ]]
-  do
-    echo "Waiting for file $1"
-    sleep 1
-  done
-}
-
-
-CERT_AUTH="/etc/openssl/ca.pem"
-CLIENT_CER="/etc/kubernetes/ssl/kubelet-server.pem"
-CLIENT_KEY="/etc/kubernetes/ssl/kubelet-server-key.pem"
-wait_for_file $CERT_AUTH
-wait_for_file $CLIENT_CER
-wait_for_file $CLIENT_KEY
-
-
-keepdoing="true"
 error=0
 
 while true
 do
-  if [[ "$keepdoing" == "true" ]]
-  then
-    echo "Waiting for kubernetes node to become ready..."
-    uncordon_ready=$( /usr/bin/kubectl get node --show-labels | grep -i "{{ nodename }}" | grep -i "ready" | grep -i "SchedulingDisabled" | wc -l )
-    if [[ "$uncordon_ready" -eq "1" ]]
-    then
-      keepdoing="false"
-      /usr/bin/kubectl uncordon {{ ansible_host }} || echo "Post start kubelet, this node was never cordoned."
-      echo "Node uncordoned, and ready!"
-    fi
-    node_ready=$( /usr/bin/kubectl get node --show-labels | grep -i "{{ nodename }}" |  grep -i " ready " | wc -l )
-    if [[ "$node_ready" -eq "1" ]]
-    then
-      keepdoing="false"
-      echo "Node become ready."
-    fi
-  fi
   set +e
-  result="$(wget --timeout 10 --tries 5 --ca-certificate $CERT_AUTH --certificate $CLIENT_CER --private-key $CLIENT_KEY --spider https://{{ ansible_host }}:10250/healthz 2>&1 | grep 'HTTP' | grep -E -o '[[:digit:]]{3}')"
-
+  result="$(curl 127.0.0.1:10248/healthz)"
   set -e
-  if [ "$result" == "200" ]
+  if [ "$result" == "ok" ]
   then
     echo "Healtcheck success"
     error=0
+    set +e
+    uncordon_ready=$( /usr/bin/kubectl get node --show-labels | grep -i "{{ nodename }}" | grep -i "ready" | grep -i "SchedulingDisabled" | wc -l )
+    if [[ "$uncordon_ready" -eq "1" ]]
+    then
+      /usr/bin/kubectl uncordon {{ ansible_host }} || echo "Node could not be uncordoned waiting next loop!"
+    fi
+    set -e
   else
     echo "Healtcheck failed"
     error=$(($error+1))
